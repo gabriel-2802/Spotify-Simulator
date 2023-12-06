62df65012cea055aacdab7798840b4095097f1ac
Cărăuleanu Valentin Gabriel <gabrielvalentine738@gmail.com>
2023-12-06 15:30:22 +0200

    cs

--- src/app/audio/Collections/AudioCollection.java
+++ src/app/audio/Collections/AudioCollection.java
@@ -4,23 +4,52 @@ import app.audio.Files.AudioFile;
 import app.audio.LibraryEntry;
 import lombok.Getter;
 
+/**
+ * The type Audio collection.
+ */
 @Getter
 public abstract class AudioCollection extends LibraryEntry {
     private final String owner;
 
-    public AudioCollection(String name, String owner) {
+    /**
+     * Instantiates a new Audio collection.
+     *
+     * @param name  the name
+     * @param owner the owner
+     */
+    public AudioCollection(final String name, final String owner) {
         super(name);
         this.owner = owner;
     }
 
+    /**
+     * Gets number of tracks.
+     *
+     * @return the number of tracks
+     */
     public abstract int getNumberOfTracks();
 
+    /**
+     * Gets track by index.
+     *
+     * @param index the index
+     * @return the track by index
+     */
     public abstract AudioFile getTrackByIndex(int index);
 
-    @Override
-    public boolean matchesOwner(String user) {
+    /**
+     *
+     * @param user the user
+     * @return if the user is the owner
+     */
+    public boolean matchesOwner(final String user) {
         return this.getOwner().equals(user);
     }
 
+    /**
+     * Contains media by creator boolean.
+     * @param creator the creator
+     * @return true if the creator is in the collection, false otherwise
+     */
     public abstract boolean containsMediaByCreator(String creator);
 }
